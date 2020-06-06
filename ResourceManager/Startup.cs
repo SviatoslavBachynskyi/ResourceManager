30a0824c9f86a31b6fa424db280fef8ad1280c47
SviatoslavBachynskyi <ba4insckij.s@gmail.com>
2020-06-06 04:48:15 +0300

    changes in Startups to call Layers injections

--- ResourceManager/Startup.cs
+++ ResourceManager/Startup.cs
@@ -19,9 +19,13 @@ namespace ResourceManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddDal(this.Configuration);
 
-            services.InjectDal(this.Configuration);
+            services.AddBll(this.Configuration);
+
+            services.AddWeb(this.Configuration);
+
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
