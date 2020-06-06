2a488f04abed1ce646881d732756fe9831c9dd94
SviatoslavBachynskyi <ba4insckij.s@gmail.com>
2020-06-06 04:40:55 +0300

    specified additional entity to load for resource

--- ResourceManager.Dal/Repositories/ResourceRepository.cs
+++ ResourceManager.Dal/Repositories/ResourceRepository.cs
@@ -1,5 +1,7 @@
-﻿using ResourceManager.Core.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using ResourceManager.Core.Models;
 using ResourceManager.Core.Repositories;
+using System.Linq;
 
 namespace ResourceManager.Dal.Repositories
 {
@@ -7,6 +9,18 @@ namespace ResourceManager.Dal.Repositories
     {
         public ResourceRepository(ResourceManagerContext context) : base(context)
         {
+        }
+
+        protected override IQueryable<Resource> ComplexEntities
+        {
+            get
+            {
+                return base.Entities
+                    .Include(r => r.MeasuringUnit)
+                    .Include(r => r.ResourceSubCategory).ThenInclude(r => r.ResourceCategory)
+                    .Include(r => r.EcologyClass)
+                    .Include(r => r.SafetyClass);
+            }
         }
     }
 }
