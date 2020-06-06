2ad4c6603a62f47e2c5edef9b4224071cabce445
SviatoslavBachynskyi <ba4insckij.s@gmail.com>
2020-06-06 04:52:24 +0300

    deleted generated tables, created table for Resource

---
+++ ResourceManager/Pages/Resource/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using ResourceManager.PageServices;
+using ResourceManager.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ResourceManager
+{
+    public class IndexModel : PageModel
+    {
+        private IResourcePageService _resourcePageService;
+
+        public IndexModel(IResourcePageService resourcePageService)
+        {
+            this._resourcePageService = resourcePageService;
+        }
+
+        public IEnumerable<ResourceViewModel> Resources { get; set; } = new List<ResourceViewModel>();
+
+        public async Task OnGetAsync()
+        {
+            this.Resources = await this._resourcePageService.GetAllAsync();
+        }
+    }
+}
